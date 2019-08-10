5aeba9d0c82af4fc91f48a95d3ab72d375bc0157
Slava <Mr-93@mail.ru>
2019-08-10 17:34:32 +0300

    added get request and setup axios (for ajax)

--- src/Components/Body/Users/Users.jsx
+++ src/Components/Body/Users/Users.jsx
@@ -1,50 +1,15 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from "axios";
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
-            [{
-            id: 1,
-            photo: 'https://cs16planet.ru/images/content/avatars/avatar648.jpg',
-            fullName: 'Dmitry',
-            status: 'I am a boze',
-            location: {city: 'Minsk', country: 'Belarus'},
-            followed: true
-        },
-            {
-                id: 2,
-                photo: 'https://cs16planet.ru/images/content/avatars/avatar648.jpg',
-                fullName: 'Slava',
-                status: 'I am a cool gay',
-                location: {city: 'RnD', country: 'Russia'},
-                followed: false
-            },
-            {
-                id: 3,
-                photo: 'https://cs16planet.ru/images/content/avatars/avatar648.jpg',
-                fullName: 'Ashot',
-                status: 'Yo oy oy yo',
-                location: {city: 'Erevan', country: 'Armenia'},
-                followed: true
-            },
-            {
-                id: 4,
-                photo: 'https://cs16planet.ru/images/content/avatars/avatar648.jpg',
-                fullName: 'Givi',
-                status: 'Show me your tits',
-                location: {city: 'Tbilisi', country: 'Georgia'},
-                followed: false
-            },
-            {
-                id: 5,
-                photo: 'https://cs16planet.ru/images/content/avatars/avatar648.jpg',
-                fullName: 'Tim',
-                status: 'Big dick are back in town',
-                location: {city: 'New-York', country: 'USA'},
-                followed: true
-            }])
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        debugger;
+            props.setUsers(response.data.items)
+        });
     }
 
     return (
@@ -54,7 +19,10 @@ const Users = (props) => {
                     <div key={u.id}>
                         <span>
                             <div>
-                                <img src={u.photo} className={styles.usersPhoto}/>
+                                <img
+                                    src={u.photos.small != null ? u.photos.small : 'https://cs16planet.ru/images/content/avatars/avatar648.jpg'}
+                                    className={styles.usersPhoto}
+                                />
                             </div>
                             <div>
                                 {u.followed ? <button onClick={
@@ -70,12 +38,8 @@ const Users = (props) => {
                         </span>
                         <span>
                             <span>
-                                <div>{u.fullName}</div>
+                                <div>{u.name}</div>
                                 <div>{u.status}</div>
-                            </span>
-                            <span>
-                                <div>{u.location.country},</div>
-                                <div>{u.location.city}</div>
                             </span>
                         </span>
                     </div>
